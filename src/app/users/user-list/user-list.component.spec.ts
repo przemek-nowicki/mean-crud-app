4a234185bef310804461bffbed7d79dd50fb0a4d
przemek <nowicki.przemek@gmail.com>
2018-06-14 12:33:49 +0200

    should render 2 users, unit test

--- src/app/users/user-list/user-list.component.spec.ts
+++ src/app/users/user-list/user-list.component.spec.ts
@@ -1,6 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import {User} from "../user";
+import {Observable, of} from "rxjs/index";
+import {UserService} from "../user.service";
+import {By} from "@angular/platform-browser";
+
+
+export class UserMockService {
+  getUsers(): Observable<User[]> {
+    return of([
+      {firstName: 'John', lastName: 'Doe', email: 'john.doe@example.io', occupation:'developer', dateOfBirth: ''},
+      {firstName: 'Steven', lastName: 'Doe', email: 'steven.doe@example.io', occupation:'developer', dateOfBirth: ''}
+    ]);
+  }
+}
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
@@ -8,7 +22,8 @@ describe('UserListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [ UserListComponent ],
+      providers: [{provide: UserService, useClass:UserMockService}]
     })
     .compileComponents();
   }));
@@ -21,5 +36,9 @@ describe('UserListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render 2 users', () => {
+    console.log('user', fixture.debugElement.queryAll(By.css('app-user-list')));
   });
 });
